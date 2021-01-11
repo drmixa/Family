3adbcd9adfd793fa50071e6cce243d967190642c
drmixa <77017898+drmixa@users.noreply.github.com>
2021-01-11 20:01:12 +0100

    demo test1
    test version

---
+++ tf_jungle_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tf_jungle_tileset" tilewidth="32" tileheight="32" tilecount="110" columns="11">
+ <image source="../../Tile Sets/tf_jungle_tileset.png" width="352" height="336"/>
+</tileset>
